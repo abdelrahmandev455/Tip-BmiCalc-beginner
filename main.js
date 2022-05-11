af229d5b4c1f47666cd051677915f422b540376d
abdelrahmandev455 <100290630+abdelrahmandev455@users.noreply.github.com>
2022-05-11 20:49:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let money = document.getElementById("money")
 let tip = document.getElementById("tip")
 let btn = document.getElementById("btn")
 let conalert = document.getElementById("conalert")
-
+let copyright = document.querySelector('.copyright') 
 btn.addEventListener("click",
 ()=>{
   
@@ -13,3 +13,4 @@ btn.addEventListener("click",
  conalert.innerText = `Total : ${tiphal} $`
 })
 
+copyright.onclick = () => { alert("All rights reseved by Abdelrahman \n Hireme: abdelrahmanshaheen2007@gmail.com") }
